16745a74fa57a282ad6552e9e1c3d19abadaf243
Daniele <daniele.cesini@gmail.com>
2025-02-04 19:12:07 +0100

    Create cowsay.sh

---
+++ Containers/cowsay.sh
@@ -0,0 +1,37 @@
+##### Launch an ubuntu container
+docker run -i -t ubuntu /bin/bash
+#---from inside the container
+apt-get update
+apt-get install fortunes
+apt-get install cowsay
+apt-get install lolcat
+export PATH=/usr/games/:$PATH
+fortune | cowsay | lolcat
+######  -------------  exit the contair
+
+docker ps -a
+docker commit ef048ccf6bd0  ubuntu_with_fortune
+docker tag ubuntu_with_fortune dcesini/hpqc_2025:ubuntu_with_fortune_1.0
+docker push dcesini/hpqc_2025:ubuntu_with_fortune_1.0
+
+docker run  -e PATH=/usr/games/:${PATH} -e LC_ALL=C  dcesini/hpqc_2025:ubuntu_with_fortune_1.0 /bin/bash -c 'fortune | cowsay | lolcat'
+
+#### Alternatively you can use a Dockerfile
+
+cat Dockerfile
+#################################
+FROM ubuntu
+
+RUN apt-get update && apt-get install -y fortunes cowsay lolcat
+
+ENV PATH=/usr/games:${PATH}
+ENV LC_ALL=C
+
+ENTRYPOINT fortune | cowsay | lolcat
+
+###############################
+
+
+docker build -t ubuntu_with_fortune4 .
+docker run  ubuntu_with_fortune4 /bin/bash -c 'fortune | cowsay | lolcat'
+

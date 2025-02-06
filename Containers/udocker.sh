41ced3928f813ace69037be349fd17fe1aba93d6
Daniele <daniele.cesini@gmail.com>
2025-02-06 10:20:13 +0100

    Create udocker.sh

---
+++ Containers/udocker.sh
@@ -0,0 +1,15 @@
+###################### INSTALL UDOCKER AS A USER ################# 
+wget https://github.com/indigo-dc/udocker/releases/download/1.3.17/udocker-1.3.17.tar.gz
+tar zxvf udocker-1.3.17.tar.gz
+export PATH=`pwd`/udocker-1.3.17/udocker:$PATH
+udocker install
+#############################
+
+######### PULL A DOCKERHUB IMAGE ##########
+udocker pull dcesini/hpqc_2025:ubuntu_with_fortune_5.0
+udocker images
+#######################
+
+######## RUN THE CONTAINER from that image ###############
+
+udocker run dcesini/hpqc_2025:ubuntu_with_fortune_5.0

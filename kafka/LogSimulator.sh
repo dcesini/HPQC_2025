85ee519286f26e378e91a7a48ba7939a0dca7e8c
Daniele <daniele.cesini@gmail.com>
2025-02-09 17:43:05 +0100

    Update LogSimulator.sh

--- kafka/LogSimulator.sh
+++ kafka/LogSimulator.sh
@@ -1,12 +1,13 @@
+sudo yum install git
 git clone https://baltig.infn.it/rossitisbeni/LogSimulator.git
 ls
 cd LogSimulator/
 git clone https://github.com/logpai/loghub
 ls loghub/
 vim logsimulator.py
-vim static/data/template.json    #change the paths!!!
+vim static/data/template.json    #change the paths!!! #ADD "Open" to SSH
 ls ./loghub/OpenSSH/
 vim static/data/template.json
-python logsimulator.py
+python logsimulator.py &
 tail -f logsimulator.log
 
